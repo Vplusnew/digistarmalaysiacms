ce72157809f3e40287db08cea28824f851b44898
Fatah Alim <fatahalim@Mac1s-MacBook-Pro.local>
2023-11-07 11:57:05 +0700

    Penambahan code

--- src/test/java/selenium/AutomationCMSDigistar/c_Configuration_Menu.java
+++ src/test/java/selenium/AutomationCMSDigistar/c_Configuration_Menu.java
@@ -1,5 +1,18 @@
 package selenium.AutomationCMSDigistar;
-
-public class c_Configuration_Menu {
-
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test; 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Sleeper;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Wait;
+@SuppressWarnings("unused")
+public class c_Configuration_Menu{
 }
